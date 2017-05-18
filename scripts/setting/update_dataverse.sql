f90db8b68a46d9f272c336ff625d13a324f11091
Piyapong Charoenwattana <piyapong.charoenwattana@gmail.com>
2017-05-18 13:52:38 -0600

    Added update root dataverse scripts.

---
+++ scripts/setting/update_dataverse.sql
@@ -0,0 +1,7 @@
+update dataverse set
+affiliation = 'Digital Initiative, University of Alberta Librareis',
+alias = 'ualib',
+dataversetype = 'ORGANIZATIONS_INSTITUTIONS',
+descriptioin = '<h3>Welcome to the University of Alberta Libraries'' Dataverse network. This service is provided to help researchers publish, analyze, distribute and preserve their data and datasets.</h3>',
+name = 'UAL',
+where id = 1
