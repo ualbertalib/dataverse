888f6a18ec1b9aff009ca1a5ee53daff1cd4b94a
piyapongch <charoenw@ualberta.ca>
2020-05-14 16:41:12 -0600

    Add postgres access port number

--- conf/docker-aio/prep_it.bash
+++ conf/docker-aio/prep_it.bash
@@ -14,7 +14,7 @@ docker build -t dv0 -f c7.dockerfile .
 # cleanup from previous runs if necessary
 docker rm -f dv
 # start container
-docker run -d -p 8084:80 -p 8080:8080 -p 9010:9009 --name dv dv0
+docker run -d -p 8084:80 -p 8080:8080 -p 9010:9009 -p 5432:5432 --name dv dv0
 # wait for glassfish to be healthy
 i_wait=0
 d_wait=10
